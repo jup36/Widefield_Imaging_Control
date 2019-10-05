e99d5e700215476ea9631b4bb3617450079e4371
gregnowak973 <greg.nowak973@gmail.com>
2019-10-04 20:30:16 -0400

    Upload

--- Utilities/InitializeAcquisition.m
+++ Utilities/InitializeAcquisition.m
@@ -1,19 +1,10 @@
 function InitializeAcquisition(app)
 
-    
-    
-
-
 
     %Start Behavioral Monitoring 
     if app.ofCamsEditField.Value>0        
         filename = CreateVideoRecordingScript([app.rootdir filesep 'Behavioral_MultiCam' filesep],...
-            app.SaveDirectoryEditField.Value,...
-            'record',0,...
-            'num_cam',app.ofCamsEditField.Value,...
-            'fps',app.FPSEditField.Value,...
-            'duration_in_sec',app.cur_routine_vals.recording_duration + (2 * app.behav_cam_options.flank_duration)...
-            );
+            app.SaveDirectoryEditField.Value,app.behav_cam_vals);
         cmd = sprintf('python "%s" && exit &',filename);
         system(cmd) 
         pause(app.behav_cam_options.flank_duration); %Start behavioral camera early 
